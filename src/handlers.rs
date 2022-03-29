00db36affdfec3f5f83a4d9444545843ebe635b6
ankit <ankitgc1@gmail.com>
2022-03-29 14:33:38 +0530

    first commit

---
+++ src/handlers.rs
@@ -0,0 +1,203 @@
+use std::{collections::HashMap, fs::File, sync::Mutex};
+
+use actix_web::{get, post, HttpResponse, Responder, web::{Data, Json, Path}};
+
+use serde::{Serialize, Deserialize};
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub password: String,
+    pub todo: Mutex<Vec<String>>,
+}
+
+
+
+#[derive(Serialize, Deserialize)]
+struct Username {
+    username: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct AddContent {
+    content: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DeleteContent {
+    idx: usize,
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct EditContent {
+    idx: usize,
+    content: String,
+}
+
+
+
+// saving the dataset
+fn save_dataset(path: String, dataset: &HashMap<String, User>) {
+    let file = File::create(path).unwrap();
+    serde_json::to_writer(file, &dataset).expect("Falied to save data!");
+}
+
+
+#[get("/{username}/todo")]
+async fn see(username: Path<Username>, state: Data<HashMap<String, User>>) -> impl Responder {
+    let user = state.get(&username.username);
+    // Pattern match to retrieve the value
+    match user {
+        Some(x) => {
+                HttpResponse::Ok().json(format!(
+                "{} your todo:- {:?}",
+                username.username, &x.todo
+            ))
+        },
+        None =>{
+                HttpResponse::Ok().json(format!(
+                "{} this user doesn't exist!",
+                username.username
+            ))
+        },
+    }
+}
+
+
+#[post("/{username}/add")]
+async fn add(username: Path<Username>, todo: Json<AddContent>, state: Data<HashMap<String, User>>) -> impl Responder {
+    let user = state.get(&username.username);
+    let todo = todo.into_inner();
+
+    // Pattern match to retrieve the value
+    match user {
+        Some(x) => {
+            {
+                let mut new_todo = x.todo.lock().unwrap();
+                new_todo.push(todo.content);
+            }
+
+            save_dataset(String::from("dataset.json"), &state);
+
+            HttpResponse::Ok().json(format!(
+            "{} your todo:- {:?}",
+            username.username, &x.todo
+            ))
+        },
+        None =>{
+            HttpResponse::Ok().json(format!(
+            "{} this user doesn't exist!",
+            username.username
+            ))
+        },
+    }
+}
+
+
+#[post("/{username}/delete")]
+async fn delete(username: Path<Username>, todo: Json<DeleteContent>, state: Data<HashMap<String, User>>) -> impl Responder {
+    let user = state.get(&username.username);
+    let todo = todo.into_inner();
+
+    // Pattern match to retrieve the value
+    match user {
+        Some(x) => {
+            {
+                let mut new_todo = x.todo.lock().unwrap();
+                new_todo.remove(todo.idx);
+            }
+
+            save_dataset(String::from("dataset.json"), &state);
+
+            HttpResponse::Ok().json(format!(
+            "{} your todo:- {:?}",
+            username.username, &x.todo
+            ))
+        },
+        None =>{
+            HttpResponse::Ok().json(format!(
+            "{} this user doesn't exist!",
+            username.username
+            ))
+        },
+    }
+}
+
+
+#[post("/{username}/edit")]
+async fn edit(username: Path<Username>, todo: Json<EditContent>, state: Data<HashMap<String, User>>) -> impl Responder {
+    let user = state.get(&username.username);
+    let todo = todo.into_inner();
+
+    // Pattern match to retrieve the value
+    match user {
+        Some(x) => {
+            {
+                let mut new_todo = x.todo.lock().unwrap();
+                new_todo[todo.idx] = todo.content;
+            }
+
+            save_dataset(String::from("dataset.json"), &state);
+
+            HttpResponse::Ok().json(format!(
+            "{} your todo:- {:?}",
+            username.username, &x.todo
+            ))
+        },
+        None =>{
+            HttpResponse::Ok().json(format!(
+            "{} this user doesn't exist!",
+            username.username
+            ))
+        },
+    }
+}
+
+#[get("/{username}")]
+async fn user_todo(username: Path<Username>, state: Data<HashMap<String, User>>) -> impl Responder {
+    let user = state.get(&username.username);
+    // Pattern match to retrieve the value
+    match user {
+        Some(_x) => {
+                HttpResponse::Ok().json(format!(
+                "Welcome {}",
+                username.username
+            ))
+        },
+        None =>{
+                HttpResponse::Ok().json(format!(
+                "{} this user doesn't exist!",
+                username.username
+            ))
+        },
+    }
+}
+
+
+
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct Dataset {
+//     pub username: String,
+//     pub userdata: User,
+// }
+
+// #[derive(Serialize, Deserialize)]
+// struct Username {
+//     username: String,
+// }
+
+// #[get("/{username}")]
+// async fn user_todo(username: Path<Username>, _: Data<Dataset>) -> impl Responder {
+//     // Ok(format!(
+//     //     "Welcome {},",
+//     //     username.username
+//     // ))
+//     // println!("{:?}", &state);
+//     HttpResponse::Ok().json(format!(
+//         "Welcome {},",
+//         username.username
+//     ))
+// }
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use actix_web::{web, App, HttpServer};
+
+mod handlers;
+use handlers::{User};
+
+use std::{fs::File, collections::HashMap};
+use std::io::BufReader;
+use serde_json::Result;
+
+
+// Reading the dataset
+fn read_dataset(path: String) -> Result<HashMap<String, User>> {
+    let file = File::open(path).expect("Failed to load file");
+    let reader = BufReader::new(file);
+    let dataset: HashMap<String, User> = serde_json::from_reader(reader)?;
+
+    Ok(dataset)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let dataset =  read_dataset(String::from("dataset.json")).unwrap();
+    // let mut dataset = Dataset { 
+    //     username: String::from("ankit"), 
+    //     userdata: User {
+    //         password: String::from("password1"),
+    //         todo : vec![],
+    //     }
+    // };
+    let dataset = web::Data::new(dataset);
+    HttpServer::new(move || {
+        App::new()
+            .service(handlers::user_todo)
+            .service(handlers::see)
+            .service(handlers::add)
+            .service(handlers::delete)
+            .service(handlers::edit)
+            .app_data(dataset.clone())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
